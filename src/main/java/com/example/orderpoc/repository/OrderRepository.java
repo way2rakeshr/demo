2f20f48b34466838b2bd3ba0139c45c3882b686e
way2rakeshr <way2rakeshr@gmail.com>
2020-06-08 17:20:36 +0530

    made changed in

---
+++ src/main/java/com/example/orderpoc/repository/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.example.orderpoc.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+
+//public	interface OrderRepository extends JpaRepository<Order, Long> {
+//	}
+
--- src/test/java/com/example/orderpoc/CloudpocApplicationTests.java
+++ src/test/java/com/example/orderpoc/CloudpocApplicationTests.java
@@ -1,10 +1,10 @@
-package com.rakesh.ratingservice;
+package com.example.orderpoc;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class RatingserviceApplicationTests {
+class CloudpocApplicationTests {
 
 	@Test
 	void contextLoads() {
