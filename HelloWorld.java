ab0aa5dfbdff186f6502d0525dd729c15c4ae8a4
rAKESH <way2rakeshr@gmail.com>
2020-07-02 19:58:08 +0530

    hello world

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+package com.example.book;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("my build");
+System.out.println("running it");
+System.out.println("success");
+	}
+
+}
--- src/main/java/com/example/orderpoc/repository/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.orderpoc.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-
-//public	interface OrderRepository extends JpaRepository<Order, Long> {
-//	}
-
